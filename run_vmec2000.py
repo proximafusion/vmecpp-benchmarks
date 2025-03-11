836b9e6e945c3eb44d06bfb78dc0de215d882d9c
Enrico Guiraud <eguiraud@proximafusion.com>
2025-03-11 17:32:02 -0600

    Better runtime logs by directly sandwiching `run`

--- run_vmec2000.py
+++ run_vmec2000.py
@@ -13,14 +13,14 @@ end_import = time.time()
 print(f"End importing simsopt.mhd. Took {end_import - start_import} s")
 
 
-def run_vmec2000(indata_file: Path):
-    vmec = simsopt.mhd.Vmec(str(indata_file))
-    vmec.run()
-
-
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("vmec_indata_file", type=Path)
     vmec_indata_file = p.parse_args().vmec_indata_file
 
-    run_vmec2000(vmec_indata_file)
+    vmec = simsopt.mhd.Vmec(str(vmec_indata_file))
+    start = time.time()
+    vmec.run()
+    runtime = time.time() - start
+
+    print(f"VMEC2000 ON {vmec_indata_file} -- runtime(s): {runtime}")
--- run_vmecpp.py
+++ run_vmecpp.py
@@ -1,6 +1,7 @@
 import vmecpp
 from pathlib import Path
 import argparse
+import time
 
 
 if __name__ == "__main__":
@@ -9,4 +10,8 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     input = vmecpp.VmecInput.from_file(args.vmec_indata_file)
+    start = time.time()
     vmecpp.run(input)
+    runtime = time.time() - start
+
+    print(f"VMECPP ON {args.vmec_indata_file} -- runtime(s): {runtime}")
