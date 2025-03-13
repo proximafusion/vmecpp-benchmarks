fde6bbb0e2121bde626eb9f226c7225ab4754493
Philipp Jurašić <166746189+jurasic-pf@users.noreply.github.com>
2025-03-13 17:05:47 +0100

    Add plotting script for benchmarks (#1)
    - Added plotting (adjusted styling to Proxima colors)
    - Minor bench.sh refactor

--- bench.sh
+++ bench.sh
@@ -2,26 +2,16 @@
 # For example, run as:
 #   bash bench.sh 2>&1 | tee logs.txt
 
-N_CORES=1
-for input_file in "data/input.precise_qa_beta1_mn12_ns99" "data/input.w7x_beta1_mn5_ns51"; do 
-  for i in 1 2 3; do
-    taskset --cpu-list 0,2,4,6 /usr/bin/time mpirun -n ${N_CORES} python run_vmec2000.py ${input_file} ${N_CORES}
-  done
+for n_cores in 1 4; do
+  for input_file in "data/input.precise_qa_beta1_mn12_ns99" "data/input.w7x_beta1_mn5_ns51"; do 
+    # Run a few times to gather statistics
+    for i in 1 2 3; do
+      taskset --cpu-list 0,2,4,6 /usr/bin/time mpirun -n ${n_cores} python run_vmec2000.py ${input_file} ${n_cores}
+    done
 
-  export OMP_NUM_THREADS=${N_CORES}
-  for i in 1 2 3; do
-    taskset --cpu-list 0,2,4,6 /usr/bin/time python run_vmecpp.py ${input_file} ${N_CORES}
-  done
-done
-
-N_CORES=4
-for input_file in "data/input.precise_qa_beta1_mn12_ns99" "data/input.w7x_beta1_mn5_ns51"; do 
-  for i in 1 2 3; do
-    taskset --cpu-list 0,2,4,6 /usr/bin/time mpirun -n ${N_CORES} python run_vmec2000.py ${input_file} ${N_CORES}
-  done
-
-  export OMP_NUM_THREADS=${N_CORES}
-  for i in 1 2 3; do
-    taskset --cpu-list 0,2,4,6 /usr/bin/time python run_vmecpp.py ${input_file} ${N_CORES}
+    export OMP_NUM_THREADS=${n_cores}
+    for i in 1 2 3; do
+      taskset --cpu-list 0,2,4,6 /usr/bin/time python run_vmecpp.py ${input_file} ${n_cores}
+    done
   done
 done
