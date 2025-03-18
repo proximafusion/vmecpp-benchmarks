0f0f3d59035c6a6a7b7074cdbe33981992ccb316
Philipp Jurašić <166746189+jurasic-pf@users.noreply.github.com>
2025-03-18 17:08:15 +0100

    Reran benchmarks due to unusually slow parallel VMEC2000 performance (#4)

--- bench.sh
+++ bench.sh
@@ -6,16 +6,14 @@
 # For example, run as:
 #   bash bench.sh 2>&1 | tee logs.txt
 
-for n_cores in 1 4; do
-  for input_file in "data/input.precise_qa_beta1_mn12_ns99" "data/input.w7x_beta1_mn5_ns51"; do 
-    # Run a few times to gather statistics
-    for i in 1 2 3; do
-      taskset --cpu-list 0,2,4,6 /usr/bin/time mpirun -n ${n_cores} python run_vmec2000.py ${input_file} ${n_cores}
-    done
+# Run a few times to gather statistics
+for i in 1 2 3 4 5; do
+  for n_cores in 1 4; do
+    for input_file in "data/input.precise_qa_beta1_mn12_ns99"  "data/input.w7x_beta1_mn5_ns51"; do
+      time mpirun --map-by core --allow-run-as-root -n ${n_cores} python run_vmec2000.py ${input_file} ${n_cores}
 
-    export OMP_NUM_THREADS=${n_cores}
-    for i in 1 2 3; do
-      taskset --cpu-list 0,2,4,6 /usr/bin/time python run_vmecpp.py ${input_file} ${n_cores}
+      export OMP_NUM_THREADS=${n_cores}
+      taskset --cpu-list 0,2,4,6 time python run_vmecpp.py ${input_file} ${n_cores}
     done
   done
 done
